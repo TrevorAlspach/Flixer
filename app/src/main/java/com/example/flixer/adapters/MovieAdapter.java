2e0e4ac015817c10ee233b265c78614a1bbf729d
TrevorAlspach <60957308+TrevorAlspach@users.noreply.github.com>
2021-01-25 14:35:09 -0500

    Changed background color to gray. Added play button on backdrop images.

--- app/src/main/java/com/example/flixer/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixer/adapters/MovieAdapter.java
@@ -2,9 +2,8 @@ package com.example.flixer.adapters;
 
 import android.content.Context;
 import android.content.res.Configuration;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,18 +30,35 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
 
     Context context;
     List<Movie> movies;
+    private final int POPULAR = 1, NOT_POPULAR = 0;
 
     public MovieAdapter(Context context, List<Movie> movies) {
         this.context = context;
         this.movies = movies;
     }
 
+    public int getItemViewType(int position){
+        if (movies.get(position).getRating() >= 5.0)
+            return POPULAR;
+        else
+            return NOT_POPULAR;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Log.d("MovieAdapter", "OnCreateViewHolder");
-        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
-        return new ViewHolder(movieView);
+        View movieView;
+        ViewHolder viewHolder;
+        if (viewType == POPULAR ) {
+            movieView = LayoutInflater.from(context).inflate(R.layout.item_movie_popular, parent, false);
+            viewHolder = new ViewHolder(movieView);
+        }
+        else {
+            movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+            viewHolder = new ViewHolder(movieView);
+        }
+        return viewHolder;
     }
 
     @Override
@@ -62,6 +78,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
+        ImageView playButton;
         ProgressBar progressBar;
 
         public ViewHolder(@NonNull View itemView) {
@@ -70,14 +87,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
             progressBar = itemView.findViewById(R.id.progressBar);
-
+            playButton = itemView.findViewById(R.id.PlayButton);
         }
 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
-            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            if ((context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) || (movie.getRating() > 5.0)){
                 imageUrl = movie.getBackdropPath();
             }
             else {
@@ -100,7 +117,8 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
                         }
                     })
                     .into(ivPoster);
-           // Glide.with(context).load(imageUrl).into(ivPoster);
+            Glide.with(context).load(movie.getplaysymbolPath()).into(playButton);
+            playButton.bringToFront();
         }
     }
 }
--- app/src/main/java/com/example/flixer/models/Movie.java
+++ app/src/main/java/com/example/flixer/models/Movie.java
@@ -13,20 +13,26 @@ public class Movie {
     String posterPath;
     String title;
     String overview;
+    int rating;
+
+    String playsymbolPath;
 
 
     public Movie(JSONObject jsonObject) throws JSONException {
+
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getInt("vote_average");
+        playsymbolPath = "https://www.freepnglogos.com/uploads/play-button-png/index-media-cover-art-play-button-overlay-5.png";
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
 
         List<Movie> movies = new ArrayList<>();
 
-        for (int i = 0; i < movieJsonArray.length(); i++){
+        for (int i = 0; i < movieJsonArray.length(); i++) {
             movies.add(new Movie(movieJsonArray.getJSONObject(i)));
         }
 
@@ -37,8 +43,8 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
 
-    public String getBackdropPath(){
-        return String.format("https://image.tmdb.org/t/p/w342/%s",backdropPath);
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w780/%s", backdropPath);
     }
 
     public String getTitle() {
@@ -48,4 +54,10 @@ public class Movie {
     public String getOverview() {
         return overview;
     }
+
+    public int getRating(){
+        return rating;
+    }
+
+    public String getplaysymbolPath(){return playsymbolPath;}
 }
